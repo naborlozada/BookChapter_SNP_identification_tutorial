eada7ffc7f32bc03a4421daa16f2c455307f51d0
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2024-07-15 15:27:37 +0200

    Create 5-Variant_calling.sh

---
+++ steps_scripts/5-Variant_calling.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+ls $choe
+
+# call variants
+bcftools mpileup -f $reference_DIR/reference_parsed.fasta $alignments_DIR/sample1.aln.dedup.raln_indels.fm.bam | bcftools call -mv -Oz -o $variants_DIR/sample1.aln.dedup.raln_indels.fm.vcf.gz;
+wait;
+sleep 2;
+
+
+# extract only SNP variants:
+bcftools view -v snps  $variants_DIR/sample1.aln.dedup.raln_indels.fm.vcf.gz  -Oz -o $variants_DIR/sample1.aln.dedup.raln_indels.fm.snps.vcf.gz
+wait;
+sleep 2;
+
+
+# filter SNPs
+bcftools view -q 0.01:minor  $variants_DIR/sample1.aln.dedup.raln_indels.fm.snps.vcf.gz | bcftools filter -i '%QUAL>20' -Oz -o $variants_DIR/sample1.aln.dedup.raln_indels.fm.snps.fltrd.vcf.gz
+wait;
+sleep 2;
+
+
+# get only biallelic SNPs
+bcftools -m2 -M2  -Oz -o $variants_DIR/sample1.aln.dedup.raln_indels.snps.fm.fltrd.biallelic.vcf.gz  $variants_DIR/sample1.aln.dedup.raln_indels.snps.fltrd.vcf.gz
+wait;
+sleep 2;
+
+# count final number of SNPs
+bcftools index -t $variants_DIR/sample1.aln.dedup.raln_indels.snps.fltrd.vcf.gz
+wait;
+sleep 2;
+
+# count SNPs:
+bcftools index -n $variants_DIR/sample1.aln.dedup.raln_indels.snps.fltrd.vcf.gz
