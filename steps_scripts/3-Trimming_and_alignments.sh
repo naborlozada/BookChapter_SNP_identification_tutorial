2147abf98acd98d12e94d51fba66f43842e859d7
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2024-07-15 14:53:00 +0200

    Create 3-Trimming_and_alignments.sh

---
+++ steps_scripts/3-Trimming_and_alignments.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/bash
+
+
+# Single FASTQ pair files (mate 1 and mate 2).
+#   sample1.R1.fastq.gz
+#   sample1.R2.fastq.gz
+
+
+# trimming
+# -----------------------------------------------------------------------------
+# Define adapters directory
+adapters_DIR="$trimmomatic/adapters";
+
+# trim adapters
+time $java22 -Djava.io.tmpdir=$tmpdir -jar $trimmomatic PE \
+    -threads 32 -phred33 \
+    -trimlog $paired_unpaired/sample1.R1_R2.trimmomatic.logfile.txt \
+    $reads_DIR/sample1.R1.fastq.gz \
+    $reads_DIR/sample1.R2.fastq.gz \
+    $paired_unpaired/sample1.R1.paired.trimclip.fastq.gz \
+    $paired_unpaired/sample1.R1.unpaired.trimclip.fastq.gz \
+    $paired_unpaired/sample1.R2.paired.trimclip.fastq.gz \
+    $paired_unpaired/sample1.R2.unpaired.trimclip.fastq.gz \
+    ILLUMINACLIP:$adapters_DIR/TruSeq3-PE.fa:2:30:10
+wait;
+sleep 2;
+
+
+# make alignment
+# -----------------------------------------------------------------------------
+bwa mem -t 20 \
+    $reference_DIR/reference_parsed.fasta \
+    $paired_unpaired/sample1.R1.paired.trimclip.fastq.gz \
+    $paired_unpaired/sample1.R2.paired.trimclip.fastq.gz \
+    -R '@RG\tID:'sample1'\tSM:'sample1'\tPL:illumina' |\
+    samtools view -b --threads 20 - > $alignments_DIR/sample1.aln.raw.bam
+wait;
+sleep 2;
+
+
+# sort alignment
+# -----------------------------------------------------------------------------
+$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 \
+    -Xmx60G -jar $picard SortSam \
+    INPUT=$alignments_DIR/sample1.aln.raw.bam \
+    OUTPUT=$alignments_DIR/sample1.aln.sorted.bam \
+    SORT_ORDER=coordinate \
+    TMP_DIR=$tmpdir
+wait;
+sleep 2;
+
+
+# Dedupping alignment
+# -----------------------------------------------------------------------------
+$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=32 \
+    -Xmx60G -jar $picard MarkDuplicates \
+    INPUT=$alignments_DIR/sample1.aln.sorted.bam \
+    OUTPUT=$alignments_DIR/sample1.aln.dedup.bam \
+    METRICS_FILE=$alignments_DIR/sample1.aln.dedup.metrics.txt \
+    TMP_DIR=$tmpdir
+wait;
+sleep 2;
+
+
+# compress the *.txt file:
+gzip $alignments_DIR/sample1.aln.dedup.metrics.txt;
+wait;
+sleep 2;
+
+
+# Verify errors in deduped alignment
+# -----------------------------------------------------------------------------
+$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 \
+    -Xmx60G -jar $picard ValidateSamFile \
+    INPUT=$alignments_DIR/sample1.aln.dedup.bam \
+    OUTPUT=$alignments_DIR/sample1.aln.dedup.ValidateSam.txt \
+    MODE=SUMMARY \
+    TMP_DIR=$tmpdir
+wait;
+sleep 2;
+
