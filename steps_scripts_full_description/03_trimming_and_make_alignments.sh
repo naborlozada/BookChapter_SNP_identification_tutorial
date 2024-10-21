6908b6dba815e5923ddf07b59c08d0006b8c07d0
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2024-10-22 01:27:11 +0200

    Update 03_trimming_and_make_alignments.sh

--- steps_scripts_full_description/03_trimming_and_make_alignments.sh
+++ steps_scripts_full_description/03_trimming_and_make_alignments.sh
@@ -24,13 +24,13 @@ BWA=/home/username/programs/bioinformatics/bwa/bwa
 reference_genome=Aedes-aegypti-LVP_AGWG_CHROMOSOMES.AaegL5_2.fasta;
 reference_dir=/home/username/book_variant_calling/steps/2_reference_genome;
 reads_dir=/home/username/raw_sequencing_data/Aedes_aegypti/population_name/*1.fastq.gz;
-reads_path=/home/username/raw_sequencing_data/Aedes_aegypti/population_name/;
+reads_path=/home/username/raw_sequencing_data/Aedes_aegypti/population_name;
 tmpdir=/home/username/TMP_DIR/;
 
 # outfiles
-paired_dir_path=/home/username/Aedes-aegypti/population_name/processing/outfiles/paired/;
-unpaired_dir_path=/home/username/Aedes-aegypti/population_name/processing/outfiles/unpaired/;
-outfiles=/home/username/Aedes-aegypti/population_name/processing/outfiles/main/;
+paired_dir_path=/home/username/Aedes-aegypti/population_name/processing/outfiles/paired;
+unpaired_dir_path=/home/username/Aedes-aegypti/population_name/processing/outfiles/unpaired;
+outfiles=/home/username/Aedes-aegypti/population_name/processing/outfiles/main;
 
 
 
@@ -69,17 +69,17 @@ do
         R2_unpaired=${R2unpaired##*/};
         
         # show the full command line used in this step, so we can check what options were used in this process when we look at the standard output file. 
-        echo ">>>conmmand_line.trimmomatic: java -jar $trimmomatic PE -threads 30 -phred33 -trimlog $outfiles$trimclip_logfile $R1 $R2  $paired_dir_path$R1_paired  $unpaired_dir_path$R1_unpaired $paired_dir_path$R2_paired  $unpaired_dir_path$R2_unpaired  ILLUMINACLIP:/ngs-data/share_tools/tools/Trimmomatic-0.38/adapters/TruSeq3-PE.fa:2:30:10  LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36";
+        echo ">>>conmmand_line.trimmomatic: java -jar $trimmomatic PE -threads 30 -phred33 -trimlog $outfiles/$trimclip_logfile $R1 $R2  $paired_dir_path/$R1_paired  $unpaired_dir_path/$R1_unpaired $paired_dir_path/$R2_paired  $unpaired_dir_path/$R2_unpaired  ILLUMINACLIP:/home/username/programs/bioinformatics/Trimmomatic-0.38/adapters/TruSeq3-PE.fa:2:30:10  LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36";
         
         # main command execution:
         time java -jar $trimmomatic PE -threads 30 -phred33 \
-            -trimlog $outfiles$trimclip_logfile \
+            -trimlog $outfiles/$trimclip_logfile \
             $R1 \
             $R2 \
-            $paired_dir_path$R1_paired \
-            $unpaired_dir_path$R1_unpaired \
-            $paired_dir_path$R2_paired \
-            $unpaired_dir_path$R2_unpaired \
+            $paired_dir_path/$R1_paired \
+            $unpaired_dir_path/$R1_unpaired \
+            $paired_dir_path/$R2_paired \
+            $unpaired_dir_path/$R2_unpaired \
             ILLUMINACLIP:/home/username/programs/bioinformatics/Trimmomatic-0.38/adapters/TruSeq3-PE.fa:2:30:10 \
             LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
         wait;
@@ -93,14 +93,14 @@ do
         NEWTAG=${R1##*/}
         ID_TAG=${NEWTAG//_1.fastq.gz/}
         BAM_R1R2_paired_outfile=`echo "$R1_paired" | sed "s/\_1_paired.trimclip.fastq/.paired.aligned_raw_reads.bam/"`
-        echo ">>>conmmand_line.BWA: $BWA mem -t 30  $reference_dir$reference_genome  $paired_dir_path$R1_paired  $paired_dir_path$R2_paired  -R '@RG\tID:$ID_TAG\tSM:$ID_TAG\tPL:illumina' | $SAMTOOLS view -b -@ 30 - >  $outfiles$BAM_R1R2_paired_outfile"
+        echo ">>>conmmand_line.BWA: $BWA mem -t 30  $reference_dir/$reference_genome  $paired_dir_path/$R1_paired  $paired_dir_path/$R2_paired  -R '@RG\tID:$ID_TAG\tSM:$ID_TAG\tPL:illumina' | $SAMTOOLS view -b -@ 30 - >  $outfiles/$BAM_R1R2_paired_outfile"
 
         # Paired-ended samples (trimmed quallity and clippled)
         time $BWA mem -t 30 \
             $reference_dir$reference_genome \
-            $paired_dir_path$R1_paired \
-            $paired_dir_path$R2_paired \
-            -R '@RG\tID:'$ID_TAG'\tSM:'$ID_TAG'\tPL:illumina' | $SAMTOOLS view -b -@ 30 - > $outfiles$BAM_R1R2_paired_outfile
+            $paired_dir_path/$R1_paired \
+            $paired_dir_path/$R2_paired \
+            -R '@RG\tID:'$ID_TAG'\tSM:'$ID_TAG'\tPL:illumina' | $SAMTOOLS view -b -@ 30 - > $outfiles/$BAM_R1R2_paired_outfile
         wait;
         sleep 2;
 
@@ -110,12 +110,12 @@ do
         echo "[3] // Sort bam reads file: SortSam //"
         echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
         BAM_SORTED_outfile=`echo "$BAM_R1R2_paired_outfile" | sed "s/\.paired.aligned_raw_reads.bam/.paired.aligned_reads_sorted.bam/"`
-        echo ">>>conmmand_line.SortSam: java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $PICARD SortSam  INPUT=$outfiles$BAM_R1R2_paired_outfile  OUTPUT=$outfiles$BAM_SORTED_outfile  SORT_ORDER=coordinate  TMP_DIR=$tmpdir"
+        echo ">>>conmmand_line.SortSam: java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $PICARD SortSam  INPUT=$outfiles/$BAM_R1R2_paired_outfile  OUTPUT=$outfiles/$BAM_SORTED_outfile  SORT_ORDER=coordinate  TMP_DIR=$tmpdir"
 
         # [SortSam] sort bam file reads (by coordinates, default & mandatory required from GATK)
         time java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $PICARD SortSam \
-            INPUT=$outfiles$BAM_R1R2_paired_outfile \
-            OUTPUT=$outfiles$BAM_SORTED_outfile \
+            INPUT=$outfiles/$BAM_R1R2_paired_outfile \
+            OUTPUT=$outfiles/$BAM_SORTED_outfile \
             SORT_ORDER=coordinate \
             TMP_DIR=$tmpdir
         wait;
@@ -128,13 +128,13 @@ do
         echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
         DUPLICATES_outfile=`echo "$BAM_SORTED_outfile" | sed "s/\.paired.aligned_reads_sorted.bam/.paired.dedup_reads.bam/"`
         DUPLICATES_metrics=`echo "$BAM_SORTED_outfile" | sed "s/\.paired.aligned_reads_sorted.bam/.paired.dedup_reads.metrics.txt/"`
-        echo ">>>conmmand_line.MarkDuplicates: java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=32 -Xmx60G -jar $PICARD MarkDuplicates  INPUT=$outfiles$BAM_SORTED_outfile  OUTPUT=$outfiles$DUPLICATES_outfile  METRICS_FILE=$outfiles$DUPLICATES_metrics  TMP_DIR=$tmpdir";
+        echo ">>>conmmand_line.MarkDuplicates: java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=32 -Xmx60G -jar $PICARD MarkDuplicates  INPUT=$outfiles/$BAM_SORTED_outfile  OUTPUT=$outfiles/$DUPLICATES_outfile  METRICS_FILE=$outfiles/$DUPLICATES_metrics  TMP_DIR=$tmpdir";
 
         # MarkDuplicates
         time java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=32 -Xmx60G -jar $PICARD MarkDuplicates \
-            INPUT=$outfiles$BAM_SORTED_outfile \
-            OUTPUT=$outfiles$DUPLICATES_outfile \
-            METRICS_FILE=$outfiles$DUPLICATES_metrics \
+            INPUT=$outfiles/$BAM_SORTED_outfile \
+            OUTPUT=$outfiles/$DUPLICATES_outfile \
+            METRICS_FILE=$outfiles/$DUPLICATES_metrics \
             TMP_DIR=$tmpdir
         wait;
         sleep 2;
@@ -144,12 +144,12 @@ do
         echo
         echo " [5] // Make an index files for the sorted bam files //"
         echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
-        echo ">>>conmmand_line.BWA: $BWA index  $outfiles$DUPLICATES_outfile"
-        $BWA index  $outfiles$DUPLICATES_outfile
+        echo ">>>conmmand_line.BWA: $BWA index  $outfiles/$DUPLICATES_outfile"
+        $BWA index  $outfiles/$DUPLICATES_outfile
         echo
         echo
-        echo ">>>conmmand_line.SAMTOOLS: $SAMTOOLS index  $outfiles$DUPLICATES_outfile"
-        $SAMTOOLS index $outfiles$DUPLICATES_outfile
+        echo ">>>conmmand_line.SAMTOOLS: $SAMTOOLS index  $outfiles/$DUPLICATES_outfile"
+        $SAMTOOLS index $outfiles/$DUPLICATES_outfile
         wait;
         sleep 2;
 
@@ -160,12 +160,12 @@ do
         echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
         BAM_DEDUP_ValidateSamFile=`echo "$DUPLICATES_outfile" | sed "s/\.paired.dedup_reads.bam/.paired.dedup_reads.ValidateSamFile.txt/"`
         
-        echo ">>>conmmand_line.ValidateSamFile: java  -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $PICARD ValidateSamFile I=$outfiles$DUPLICATES_outfile  O=$outfiles$BAM_DEDUP_ValidateSamFile   MODE=SUMMARY   TMP_DIR=$tmpdir";
+        echo ">>>conmmand_line.ValidateSamFile: java  -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $PICARD ValidateSamFile I=$outfiles/$DUPLICATES_outfile  O=$outfiles/$BAM_DEDUP_ValidateSamFile   MODE=SUMMARY   TMP_DIR=$tmpdir";
 
         # validate the format and other feature that can cause errors during the next analysis.
         time java -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx50G -jar $PICARD ValidateSamFile \
-            I=$outfiles$DUPLICATES_outfile \
-            O=$outfiles$BAM_DEDUP_ValidateSamFile \
+            I=$outfiles/$DUPLICATES_outfile \
+            O=$outfiles/$BAM_DEDUP_ValidateSamFile \
             MODE=SUMMARY \
             TMP_DIR=$tmpdir
         wait;
