21d88e9235cd2b6a72137d20bf87237b3a406c4f
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2025-09-02 09:08:47 +0200

    Add files via upload

---
+++ scripts_tutorial/scripts_description/04_indel_realigment.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/bash
+
+
+
+# // Correction of aligments for each single sample in a single Directory //
+
+# Bash script to process several alignments in a LOOP. That is, a single run per sample. This is particularly useful when there is only 1 computer and/or 1 server several threads that can be used.
+# It is important to mention that the first two steps are the main processes that run in parallel, while the rest does run with one thread.
+# IMPORTANT: Do the proper changes in the directory full path names for programs/scripts ('programs' section) used to run this bash script, and also for the infiles, outfiles (if required), and output directory names. 
+#
+# Run this script as follow:
+#       nohup bash process_all_samples.loop_job.sh 2>> process_all_samples.loop_job.stderr.log &> process_all_samples.loop_job.stderr.log &
+
+
+# variables:
+# $tmpdir     : location of a directory where temporal files will be created. 
+# $java8      : location of the binary java file (version 8)
+# $gatk381    : location of GATK (version 3.8.1)  
+
+# $reference_DIR  : location of the reference genome (fasta) and associated index files.
+# $alignments_DIR : location of BAM alignment files.
+
+
+
+# // paths & programs //
+# --------------------------------------------------
+# programs
+java8=      # write full path directory location of java version 8 for GATK 3.8 
+java22=     # write full path directory location of java version 22 for Picard 
+gatk381=    # write full path directory location of GATK 3.8
+
+# infiles
+reference_genome=Aedes-aegypti-LVP_AGWG_CHROMOSOMES.AaegL5_2.fasta;
+reference_dir=/home/username/book_variant_calling/steps/2_reference_genome;
+bams_dir=/home/username/raw_sequencing_data/Aedes_aegypti/population_name/*.paired.dedup_reads.bam;
+bams_path=/home/username/raw_sequencing_data/Aedes_aegypti/population_name/;
+tmpdir=/home/username/TMP_DIR/;
+
+
+
+
+echo
+echo
+echo
+echo JOB_STARTED: `date`
+echo
+echo `pwd`
+echo
+echo
+echo
+echo "// [MAIN] PROCESSING REALINGMENT CORRECTION PER SAMPLE //"
+echo "####################################################################################################################################################################################"
+
+for BAMFILE in $reads_dir
+do
+        echo
+        echo "[1] // Recalibration of the alignment based on Indel realigments //"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+       FILENAME=$(basename ${BAMFILE} .bam);
+        
+        # identify conflictive signal regions (run it using several threads):
+        echo "$java8 -XX:ParallelGCThreads=31 -Xmx40G -jar $gatk381 -T RealignerTargetCreator -R $reference_dir/$reference_genome -I $bams_path/$BAMFILE -o $bams_path/${FILENAME}.indels.intervals --num_threads 15";
+        
+        # main
+        $java8 -XX:ParallelGCThreads=31 -Xmx40G -jar $gatk381 \
+            -T RealignerTargetCreator \
+            -R $reference_dir/$reference_genome \
+            -I $bams_path/$BAMFILE \
+            -o $bams_path/${FILENAME}.indels.intervals \
+            --num_threads 15
+        wait;
+        sleep 2;
+        
+        
+        echo "[2] // Make correction on those regions //"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        echo "$java8 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=1 -Xmx40G -jar $gatk381 -T IndelRealigner -R $reference_dir/$reference_genome -I $bams_path/$BAMFILE -known indels.db.vcf.gz -targetIntervals $bams_path/${FILENAME}.indels.intervals -o $bams_path/${FILENAME}.raln_indels.bam";
+        
+        # main:        
+        $ $java8 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=1 -Xmx40G -jar $gatk381 \
+            -T IndelRealigner \
+            -R $reference_dir/$reference_genome \
+            -I $bams_path/$BAMFILE \
+            -known indels.db.vcf.gz \                   # this line is optional
+            -targetIntervals $bams_path/${FILENAME}.indels.intervals \
+            -o $bams_path/${FILENAME}.raln_indels.bam
+        wait;
+        sleep 2;
+        
+        
+        echo "[3] // First check: Verify errors after the indel realignment //"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        echo "$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20  -Xmx60G -jar $picard ValidateSamFile INPUT=$bams_path/${FILENAME}.raln_indels.bam OUTPUT=$bams_path/${FILENAME}.raln_indels.ValidateSam.txt MODE=SUMMARY TMP_DIR=$tmpdir";
+        
+        # main:
+        $java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20  -Xmx60G -jar $picard ValidateSamFile \
+            INPUT=$bams_path/${FILENAME}.raln_indels.bam \
+            OUTPUT=$bams_path/${FILENAME}.raln_indels.ValidateSam.txt \
+            MODE=SUMMARY \
+            TMP_DIR=$tmpdir
+        wait;
+        sleep 2;
+        
+        
+        echo "[4] // Make correction if errors were found in the realignment //"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        echo "$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20  -Xmx60G -jar $picard FixMateInformation  INPUT=$bams_path/${FILENAME}.raln_indels.bam  OUTPUT=$bams_path/${FILENAME}.raln_indels.fm.bam  ADD_MATE_CIGAR=true";
+        
+        # main
+        $java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 \
+            -Xmx60G -jar $picard FixMateInformation \
+            INPUT=$bams_path/${FILENAME}.raln_indels.bam \
+            OUTPUT=$bams_path/${FILENAME}.raln_indels.fm.bam \
+            ADD_MATE_CIGAR=true
+        wait;
+        sleep 2;
+        
+        
+        echo "[5] // Second check: Verify errors after the indel realignment //"
+        echo "--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        echo "$java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 -Xmx60G -jar $picard ValidateSamFile INPUT=$bams_path/${FILENAME}.raln_indels.fm.bam OUTPUT=$bams_path/${FILENAME}.raln_indels.fm.ValidateSam.txt MODE=SUMMARY TMP_DIR=$tmpdir";        
+        
+        # main:
+        $java22 -Djava.io.tmpdir=$tmpdir -XX:ParallelGCThreads=20 \
+            -Xmx60G -jar $picard ValidateSamFile \
+            INPUT=$bams_path/${FILENAME}.raln_indels.fm.bam \
+            OUTPUT=$bams_path/${FILENAME}.raln_indels.fm.ValidateSam.txt \
+            MODE=SUMMARY \
+            TMP_DIR=$tmpdir
+        wait;
+        sleep 2;
+        
+        # The content of the second check file should be "No errors found.". 
+
+
+        echo
+        endtime=`date +"%s"`;
+        duration_sec=$((endtime - starttime));
+        duration_min=$(( $duration_sec / 60 ));
+        duration_hrs=$( echo $duration_sec / 3600 | bc -l );    # add precision count
+        wait;
+        sleep 2;
+
+
+        echo
+        echo
+        echo JOB_ENDED: `date`
+        echo
+        echo
+        echo "STAT:startTime:$starttime";
+        echo "STAT:doneTime:$endtime";
+        echo "STAT:runtime_sec:$duration_sec";
+        echo "STAT:runtime_min:$duration_min";
+        echo "STAT:runtime_hrs:$duration_hrs";
+        echo
+        echo
+
+        endtime=0;
+        duration_sec=0;
+        duration_min=0;
+        duration_hrs=0;
+done
+
+echo
+echo
+echo "####################################################################################################################################################################################"
+echo
+echo
+echo JOB_ENDED: `date`
+echo
+echo
---
+++ scripts_tutorial/scripts_description/05_variant_calling.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+
+
+# This script will call variants (SNPs and INDELs) for a set of samples in a population. In our 4 examples, 2 pairs belong to two different populations, Thies (Senegal)  and Tapachula (Mexico).
+# Although these are two examples of two populations, the number of samples per population should be expected to be higher, so we can have a better quality call (identification) of each variant. However, this is an ideal situation and we know that sometimes this is no feasible.
+
+# First, we have to create two TXT files with the names of the BAM files of each population separately. Next, use them to make the calls.
+
+vi mexico.tapachula.txt  # (SRR11196649, SRR11196650)
+vi senegal.thies.txt     # (SRR11006725, SRR11006726)
+
+# Second, set variables for infiles:
+# reference_DIR : location of the reference genome (fasta) and associated index files.
+# variants_DIR  : location where the file with the variants (variant calling file, VCF) will be saved.
+
+reference_genome=/home/username/book_variant_calling/steps/2_reference_genome/Aedes-aegypti-LVP_AGWG_CHROMOSOMES_AaegL5.parsed;
+DIRFILES=/home/username/Aedes-aegypti/population_name/processing/outfiles/main;
+variants_DIR=/home/username/Aedes-aegypti/population_name/variant_calling;
+
+
+# ------------------------------------------------------------------------------------------------
+#                                 /// MAIN STEPS ///
+# ------------------------------------------------------------------------------------------------
+
+# call variants
+bcftools mpileup -f ${reference_genome}.fasta  -b $DIRFILES/mexico.tapachula.txt | bcftools call -mv -Oz -o $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.vcf.gz;
+wait;
+sleep 2;
+
+# Suggestion (recommended): Remove SNPs with a close proximity to indes of 10 bp:
+bcftools filter --threads 3 --SnpGap 10 -Oz -o $variants_DIR/mySNPs.closeprox_rmvd.vcf.gz  $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.vcf.gz
+wait;
+
+
+# Extract only SNP variants:
+bcftools view -v snps  $variants_DIR/mySNPs.closeprox_rmvd.vcf.gz  -Oz -o $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.vcf.gz
+wait;
+sleep 2;
+
+
+# filter SNPs by retaining only SNPs with a minor allele frequency of 0.01 && mapping quality bigger than 20:
+bcftools view -q 0.01:minor  $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.vcf.gz | bcftools filter -i '%QUAL>20' -Oz -o $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.fltrd.vcf.gz
+wait;
+sleep 2;
+
+
+# Extract only biallelic SNPs
+bcftools -m2 -M2  -Oz -o $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.fltrd.biallelic.vcf.gz  $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.fltrd.vcf.gz
+wait;
+sleep 2;
+
+# Count number of SNPs: first, index the VCF file (option: index -t), then count then (option: index -n). This will create a file with the same name of the VCF file, but with a file extension *vcf.gz.tbi
+bcftools index -t $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.fltrd.biallelic.vcf.gz
+wait;
+sleep 2;
+
+# count SNPs:
+bcftools index -n $variants_DIR/mexico.tapachula.aln.dedup.raln_indels.fm.snps.fltrd.biallelic.vcf.gz
+
+
+# Done!
+# This procedure has to be repeated for the population of Thies (Senegal) by copy-n-paste the code and replacing the population name across the whole code or you can implement a loop bash script to run it automatically one after another, so no need to copy-n-paste all the code. More in the 'Parallelization' section.
+# There are more filtering options to clean the SNPs dataset. For example:
+
+
+
